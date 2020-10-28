ee613831093df8c33a6ce34264f0087a05aa9b6c
ArielColqueH <arielcolque444@gmail.com>
2020-10-28 12:37:13 -0400

    especialist with list dynamic done

---
+++ src/app/models/ListaEspecialista.ts
@@ -0,0 +1,6 @@
+export class ListEspecialist {
+  especialist_id: number;
+  especialist_avatar: string;
+  especialist_name: string;
+  especialist_score: number;
+}
--- src/app/modules/home/pages/home/asistencia-base/asistencia-medica/lista-especialidades/lista-especialistas/lista-especialistas.component.ts
+++ src/app/modules/home/pages/home/asistencia-base/asistencia-medica/lista-especialidades/lista-especialistas/lista-especialistas.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ListEspecialist } from "src/app/models/ListaEspecialista";
 
 @Component({
-  selector: 'app-lista-especialistas',
-  templateUrl: './lista-especialistas.component.html',
-  styleUrls: ['./lista-especialistas.component.scss']
+  selector: "app-lista-especialistas",
+  templateUrl: "./lista-especialistas.component.html",
+  styleUrls: ["./lista-especialistas.component.scss"],
 })
 export class ListaEspecialistasComponent implements OnInit {
+  listaEspecialista: ListEspecialist[] = [
+    {
+      especialist_id: 1,
+      especialist_avatar: "A",
+      especialist_name: "Ariel Colque Herrera",
+      especialist_score: 4.2,
+    },
+    {
+      especialist_id: 2,
+      especialist_avatar: "V",
+      especialist_name: "Lizbeth vania Vera",
+      especialist_score: 4.8,
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
