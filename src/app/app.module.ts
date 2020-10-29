70b297f2129d655d8a506a2d708ddf81376e6ddf
Mariana Cardozo <mcardozocosta@hotmail.com>
2020-10-28 23:50:19 -0400

    Error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,10 +25,11 @@ import {
   LayoutGapStyleBuilder,
   LayoutGapDirective,
 } from "@angular/flex-layout";
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HomeModule],
+  imports: [BrowserModule, AppRoutingModule, HomeModule, NoopAnimationsModule],
   providers: [
     StyleUtils,
     StylesheetMap,
