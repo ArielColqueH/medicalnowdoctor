ebd07a894ad94f97adf5f8f55c2f29b679009a76
ArielColqueH <arielcolque444@gmail.com>
2020-10-31 18:58:10 -0400

    configuraciones page

--- src/app/modules/home/pages/home/configuraciones/configuraciones.component.ts
+++ src/app/modules/home/pages/home/configuraciones/configuraciones.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { User } from "src/app/models/user";
 @Component({
-  selector: 'app-configuraciones',
-  templateUrl: './configuraciones.component.html',
-  styleUrls: ['./configuraciones.component.scss']
+  selector: "app-configuraciones",
+  templateUrl: "./configuraciones.component.html",
+  styleUrls: ["./configuraciones.component.scss"],
 })
 export class ConfiguracionesComponent implements OnInit {
+  selectedValueCombobox: string;
+  ciudad_combo: String[] = ["La Paz", "Cochabamba", "Santa Cruz"];
+  user = new User();
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
