976d1a50c43b5ff276499b8fe07624cd32e5c916
ArielColqueH <arielcolque444@gmail.com>
2020-10-31 21:05:21 -0400

    pagina de laboratorio agregada

---
+++ src/app/models/laboratory.ts
@@ -0,0 +1,8 @@
+export class Laboratory {
+  laboratoryId: number;
+  laboratoryName: string;
+  laboratoryDoctor: string;
+  laboratoryDoctorSpecialist: string;
+  laboratoryDate: string;
+  constructor() {}
+}
--- src/app/modules/home/pages/home/laboratorios/laboratorios.component.ts
+++ src/app/modules/home/pages/home/laboratorios/laboratorios.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Laboratory } from "src/app/models/laboratory";
+import { ListEspecialist } from "src/app/models/ListaEspecialista";
 
 @Component({
-  selector: 'app-laboratorios',
-  templateUrl: './laboratorios.component.html',
-  styleUrls: ['./laboratorios.component.scss']
+  selector: "app-laboratorios",
+  templateUrl: "./laboratorios.component.html",
+  styleUrls: ["./laboratorios.component.scss"],
 })
 export class LaboratoriosComponent implements OnInit {
+  laboratoryList: Laboratory[] = [
+    {
+      laboratoryId: 1,
+      laboratoryName: "Electrocardiograma",
+      laboratoryDoctor: "Tony Stark",
+      laboratoryDoctorSpecialist: "Cardiologo",
+      laboratoryDate: "11/12/2020",
+    },
+    {
+      laboratoryId: 2,
+      laboratoryName: "Analisis",
+      laboratoryDoctor: "Rey richards",
+      laboratoryDoctorSpecialist: "Dentista",
+      laboratoryDate: "15/12/2020",
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
