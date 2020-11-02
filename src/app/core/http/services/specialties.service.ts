389cf5eddf95bc5bcbf4f20c6e72332bb5a6b76d
ArielColqueH <arielcolque444@gmail.com>
2020-11-02 01:00:24 -0400

    login tokens done

--- src/app/core/http/services/specialties.service.ts
+++ src/app/core/http/services/specialties.service.ts
@@ -1,17 +1,21 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { SpecialtyModel } from 'src/app/models/specialty-model';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { SpecialtyModel } from "src/app/models/specialty-model";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class SpecialtiesService {
-
-  constructor(private  _http: HttpClient) { }
+  constructor(private _http: HttpClient) {}
   URL = "http://localhost:8008/api/v1/specialties";
-
-  listaSpeciality(){
-    return this._http.get<SpecialtyModel[]>(this.URL);
+  listaSpeciality() {
+    var tokenUser = localStorage.getItem("JWT_TOKEN");
+    const reqHeader = new HttpHeaders({
+      Authorization: "bearer " + tokenUser,
+    });
+    return this._http.get<SpecialtyModel[]>(this.URL, {
+      headers: reqHeader,
+    });
   }
 }
