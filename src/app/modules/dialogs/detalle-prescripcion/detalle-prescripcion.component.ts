6b8b860b4b56a18b5ada797ce8315c7b305261b0
ArielColqueH <arielcolque444@gmail.com>
2020-11-14 11:59:30 -0400

    prescription finished

--- src/app/modules/dialogs/detalle-prescripcion/detalle-prescripcion.component.ts
+++ src/app/modules/dialogs/detalle-prescripcion/detalle-prescripcion.component.ts
@@ -7,22 +7,35 @@ import { Prescription } from "src/app/models/prescription";
   styleUrls: ["./detalle-prescripcion.component.scss"],
 })
 export class DetallePrescripcionComponent implements OnInit {
-  prescriptionList: Prescription[] = [
-    {
-      prescriptionId: 1,
-      prescriptionName: "Vitagripin",
-      prescriptionDetail: "5 pastillas diarias",
-      prescriptionQuantity: 10,
-    },
-    {
-      prescriptionId: 2,
-      prescriptionName: "Alikal LCH",
-      prescriptionDetail: "4 pastillas diarias",
-      prescriptionQuantity: 15,
-    },
-  ];
+  prescriptionList: Prescription[] = [];
+  descripcion: string;
 
   constructor() {}
 
   ngOnInit() {}
+  agregaralista() {
+    var aux = {
+      prescriptionId: 0,
+      prescriptionName: "",
+      prescriptionDetail: "",
+      prescriptionQuantity: 0,
+    };
+    this.prescriptionList.push(aux);
+  }
+  eliminardelista(listaid: number) {
+    console.log(listaid);
+    this.prescriptionList.splice(listaid, 1);
+  }
+  enviarprescripcion() {
+    console.log(this.descripcion);
+    this.prescriptionList.forEach((element) => {
+      console.log(
+        element.prescriptionName +
+          " " +
+          element.prescriptionDetail +
+          " " +
+          element.prescriptionQuantity
+      );
+    });
+  }
 }
